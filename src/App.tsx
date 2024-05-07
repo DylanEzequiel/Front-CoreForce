ea91e7a7c2066f2e506edac00488a5f8c725a598
dylan correa <D.correa.E05@gmail.com>
2024-05-07 10:32:01 -0300

    Routes App conf & del Comp/View

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-// import { Route, Routes } from 'react-router'
+import { Route, Routes } from 'react-router'
 import { NavBar } from './Components/NavBar/NavBar'
+import { Home } from './view/home/Home'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
  
@@ -7,10 +9,13 @@ function App() {
   return (
     <div>
       <NavBar></NavBar>
-      {/* aca van las rutas a las que hacemos en Pages -Dylan */}
-      {/* <Routes>
-        <Route path='/' element={<h1>Tootkit</h1>}></Route>
-      </Routes> */}
+      {/* aca van las rutas a las que hacemos en Pages -Dylan  */}
+      <BrowserRouter>
+        <Routes>
+          <Route index path='/home' element={<Home></Home>}></Route>
+        </Routes>
+      
+      </BrowserRouter>
 
     </div>
   )
