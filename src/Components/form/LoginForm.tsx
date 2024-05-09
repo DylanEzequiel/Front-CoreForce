9e9bcf3e7e680d6ea7d27632752df31ea8db44e7
dylan correa <D.correa.E05@gmail.com>
2024-05-09 17:55:19 -0300

    Toastify Login&Reg

--- src/Components/form/LoginForm.tsx
+++ src/Components/form/LoginForm.tsx
@@ -3,17 +3,18 @@ import { useForm } from '../../hooks/useForm'
 import { Link, useNavigate } from 'react-router-dom'
 import ValidateLogin, { IErrorsLogin } from '../../helpers/ValidateLogin'
 import axios from 'axios'
+import { toast } from 'react-toastify'
 
 function LoginForm():React.ReactElement {
     const apiUrl=process.env.API_URL
     const navigate = useNavigate()
+   
     //obtengo funcionalidades del custom hook de juampi (que buen hook loco)
     const {onInputChange,
         formState,
         touched,
         email,
-        password,
-        confirmPassword}=
+        password}=
     useForm({email:"",
     password:"",
     confirmPassword:"",})
@@ -40,11 +41,14 @@ function LoginForm():React.ReactElement {
         await axios.post(`http://localhost:3000/auth/login`,{email:formState.email,password:formState.password})
         .then((data)=>{
           console.log(data.data)
+            toast.success("Login succes! welcome back")
           return data.data})
         .then((user)=>{sessionStorage.setItem("UserToken",user.token)
           navigate("/")
         })
-        .catch(error=>alert(error))
+        .catch(error=>{console.error(error)
+          toast.warn("Ups! something went wrong")
+        })
     }
   return (
     <div className="dark:border-gray-700 bg-white dark:bg-gray-800 shadow m-60 md:mt-0 xl:p-0 dark:border rounded-lg w-full sm:max-w-2xl">
@@ -99,33 +103,10 @@ function LoginForm():React.ReactElement {
             </label>
           </div>
 
-          <div className="relative mt-4 w-full">
-            <input
-              id="confirmPassword"
-              type="password"
-              name="confirmPassword"
-              placeholder=" "
-              className={`peer w-full px-4 py-2 pt-6 font-light bg-white border-2 rounded-md outline-none transition pl-4  ${
-                errorsLog.confirmPassword ? "border-red-500" : "border-neutral-500"
-              }`}
-              onChange={onInputChange}
-              value={confirmPassword}
-            />
-            {errorsLog.confirmPassword && (
-              <span className="text-red-500">{errorsLog.confirmPassword}</span>
-            )}
-            <label
-              htmlFor="confirmPassword"
-              className="top-5 left-4 z-10 absolute text-md transform origin-[0] -translate-y-3 peer-focus:-translate-y-4 peer-placeholder-shown:translate-y-0 duration-150 peer-placeholder-shown:scale-100 peer-focus:scale-75"
-            >
-              Repeat password
-            </label>
-          </div>
 
           <button
             type="submit"
-            className="inline-block bg-secondary focus:ring-opacity-50 shadow-sm focus:shadow-sm hover:shadow-md mt-4 py-3 rounded-sm w-full font-semibold text-center text-lg text-white transition duration-200"
-          >
+            className="inline-block bg-secondary focus:ring-opacity-50 shadow-sm focus:shadow-sm hover:shadow-md mt-4 py-3 rounded-sm w-full font-semibold text-center text-lg text-white transition duration-200">
             Login 
           </button>
           </div>
