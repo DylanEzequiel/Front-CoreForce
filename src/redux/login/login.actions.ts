633b3bf46b76fcabd108451dea3c70ac479c4eaf
rafael valbuena <artillero.satanas@gmail.com>
2024-05-10 11:47:32 -0500

    fusionado con trabajo anterior

--- src/redux/login/login.actions.ts
+++ src/redux/login/login.actions.ts
@@ -1,6 +1,44 @@
-export const loginAction = () => (dispatch:any) => {
-dispatch({
-      type:"TEST",
-      payload:true
-})
+import axios from "axios";
+
+const url: string = import.meta.env.VITE_API_URL || '';
+
+interface ILogin {
+  email: string,
+  password: string
 }
+
+
+export const loginAction = ({ email, password }: ILogin) => {
+  return async (dispatch: any) => {
+    try {
+      const body = { email, password };
+      const data  = await axios.post(`${url}auth/login`, body);
+      console.log(data)
+      if (data.status === 201) {
+        localStorage.setItem("userData", JSON.stringify({ status: "logged" }));
+        dispatch({
+          type: "LOGIN",
+          payload: true,
+        });
+        console.log("conectado");
+      } 
+    } catch (error: any) {
+      dispatch({
+        type: "ERROR",
+        payload: error,
+      });
+      console.log(error.message);
+    }
+  };
+};
+
+export const logout = () => {
+  return (dispatch: any) => {
+    localStorage.removeItem("userData");
+    dispatch({
+      type: "LOGOUT",
+      payload: false,
+    });
+    console.log("deslogeado");
+  };
+};
