cb8ca258e143283165a16d316ccd8decf6287f08
dylan correa <D.correa.E05@gmail.com>
2024-05-13 11:26:12 -0300

    SmallChanges/Color/navigate

--- src/Components/gallery/Gallery.tsx
+++ src/Components/gallery/Gallery.tsx
@@ -2,49 +2,49 @@
 
 const Gallery: React.FC = () => {
   return (
-    <section className="py-5 my-5">
+    <section className="my-5 py-5">
        
-      <div className="container mx-auto py-10">
-        <h2 className="text-center text-5xl text-slate-800 font-semibold">Meet Your Trainer</h2> 
-        <p className="text-center font-semibold text-xl text-gray-500">Our training programs are crafted with your fitness journey in mind, ensuring personalized guidance and expert support every step of the way.</p>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 my-16">
+      <div className="mx-auto py-10 container">
+        <h2 className="font-semibold text-5xl text-center text-slate-800">Meet Your Trainer</h2> 
+        <p className="font-semibold text-center text-gray-500 text-xl">Our training programs are crafted with your fitness journey in mind, ensuring personalized guidance and expert support every step of the way.</p>
+        <div className="gap-3 grid grid-cols-1 lg:grid-cols-3 my-16">
 
-          <div className="aspect-square relative rounded-md grayscale hover:filter-none">
+          <div className="relative rounded-md aspect-square grayscale hover:filter-none">
             <div className="shadow-lg">
               <img src="/img/trainer1.jpg" alt="" 
-                className="aspect-[5/7] rounded-md object-cover transition-all w-full h-full"
+                className="rounded-md w-full h-full transition-all aspect-[5/7] object-cover"
               />
             </div>
 
-            <div className="flex flex-col justify-center items-center absolute inset-0 opacity-0 border border-gray-300 rounded-sm transition-all duration-500 py-4 px-1 hover:inset-5 hover:opacity-100">
-              <h3 className="text-3xl text-orange-800 font-bold">CoreForce</h3>
-              <p className="text-pretty text-lg text-center font-semibold">Explore the mastery behind each workout session in our trainer</p>
+            <div className="absolute inset-0 hover:inset-5 flex flex-col justify-center items-center border-gray-300 opacity-0 hover:opacity-100 px-1 py-4 border rounded-sm transition-all duration-500">
+              <h3 className="font-bold text-3xl text-orange-800">CoreForce</h3>
+              <p className="font-semibold text-center text-lg text-pretty text-white">Explore the mastery behind each workout session in our trainer</p>
             </div>
           </div>
 
-          <div className="aspect-square relative rounded-md grayscale hover:filter-none">
+          <div className="relative rounded-md aspect-square grayscale hover:filter-none">
             <div className="shadow-lg">
               <img src="/img/trainer3.jpg" alt="" 
-                className="aspect-[5/7] rounded-md object-cover transition-all w-full h-full"
+                className="rounded-md w-full h-full transition-all aspect-[5/7] object-cover"
               />
             </div>
 
-            <div className="flex flex-col justify-center items-center absolute inset-0 opacity-0 border border-gray-300 rounded-sm transition-all duration-500 py-4 px-1 hover:inset-5 hover:opacity-100">
-              <h3 className="text-3xl text-orange-800 font-bold">CoreForce</h3>
-              <p className="text-pretty text-lg text-center font-semibold">Explore the mastery behind each workout session in our trainer</p>
+            <div className="absolute inset-0 hover:inset-5 flex flex-col justify-center items-center border-gray-300 opacity-0 hover:opacity-100 px-1 py-4 border rounded-sm transition-all duration-500">
+              <h3 className="font-bold text-3xl text-orange-800">CoreForce</h3>
+              <p className="font-semibold text-center text-lg text-pretty text-white">Explore the mastery behind each workout session in our trainer</p>
             </div>
           </div>
 
-          <div className="aspect-square relative rounded-md grayscale hover:filter-none">
+          <div className="relative rounded-md aspect-square grayscale hover:filter-none">
             <div className="shadow-lg">
               <img src="/img/trainer2.jpg" alt="" 
-                className="aspect-[5/7] rounded-md object-cover transition-all w-full h-full"
+                className="rounded-md w-full h-full transition-all aspect-[5/7] object-cover"
               />
             </div>
 
-            <div className="flex flex-col justify-center items-center absolute inset-0 opacity-0 border border-gray-300 rounded-sm transition-all duration-500 py-4 px-1 hover:inset-5 hover:opacity-100">
-              <h3 className="text-3xl text-orange-800 font-bold">CoreForce</h3>
-              <p className="text-pretty text-lg text-center font-semibold">Explore the mastery behind each workout session in our trainer</p>
+            <div className="absolute inset-0 hover:inset-5 flex flex-col justify-center items-center border-gray-300 opacity-0 hover:opacity-100 px-1 py-4 border rounded-sm transition-all duration-500">
+              <h3 className="font-bold text-3xl text-orange-800">CoreForce</h3>
+              <p className="font-semibold text-center text-lg text-pretty text-white">Explore the mastery behind each workout session in our trainer</p>
             </div>
           </div>
 
