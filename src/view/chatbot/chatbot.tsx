47e3f2746e7ba702e377d5ba3d40e1c759949dc2
FabrizioZeballos <zeballosfab@gmail.com>
2024-05-28 00:04:06 -0300

    Añadir estilos a chatbot

--- src/view/chatbot/chatbot.tsx
+++ src/view/chatbot/chatbot.tsx
@@ -58,7 +58,6 @@ export const Chatbot = () => {
       ],
     },
   ]);
-  const [buttonBgColor, setButtonBgColor] = useState("transparent");
 
   const chatContainerRef = useRef<HTMLDivElement>(null);
 
@@ -106,14 +105,13 @@ export const Chatbot = () => {
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
-    setButtonBgColor(e.target.value.trim() === "" ? "transparent" : "#1e293b");
   };
 
   return (
     <div>
       <div className="chatbot flex flex-col justify-end items-center max-h-[calc(100vh-10rem)] overflow-y-auto">
         <section
-          className="search-section bottom-10 relative flex flex-col items-center w-full overflow-y-auto"
+          className="search-section bottom-10 relative flex flex-col items-center w-full overflow-y-auto pt-20"
           ref={chatContainerRef}
         >
           <div className="search-result mb-10 relative w-1/2">
@@ -130,18 +128,20 @@ export const Chatbot = () => {
           </div>
         </section>
       </div>
-      <div className="input-container w-full ">
-        <div className="relative w-full flex justify-center">
+      <div className="input-container w-full flex justify-center ">
+        <div className="relative w-1/2">
           <input
             value={value}
             placeholder="Enter a prompt here"
             onChange={handleInputChange}
-            className="w-1/2 p-4 bg-gray3 rounded-3xl border text-gray-800 placeholder-gray-500 focus:outline-none focus:bg-gray4"
+            className="w-full p-4 bg-gray3 rounded-3xl border text-gray-800 placeholder-gray-500 focus:outline-none focus:bg-gray4 resize-x"
           />
           <button
             onClick={getResponse}
-            className="absolute right-2 top-2 bottom-2 text-white p-2 rounded-full bg-transparent hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors duration-100"
-            style={{ backgroundColor: buttonBgColor }}
+            className="absolute right-2 top-2 bottom-2 text-white p-2 rounded-full bg-transparent hover:bg-gray-800 focus:outline-none transition-colors duration-100 "
+            style={{
+              backgroundColor: value ? "#1e293b" : "transparent",
+            }}
           >
             <IoMdSend className="m-auto text-xl" />{" "}
           </button>
